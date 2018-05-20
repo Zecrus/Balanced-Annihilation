591853b0fe18539cf12b79bffd0dc3a443f6b923
Zecrus <juneauj@rose-hulman.edu>
2018-05-19 21:47:54 -0700

    Update corrship.lua

--- units/CoreShips/corrship.lua
+++ units/CoreShips/corrship.lua
@@ -14,7 +14,7 @@ return {
 		collisionvolumescales = "28 28 71",
 		collisionvolumetype = "CylZ",
 		corpse = "DEAD",
-		description = "Support Frigate (Depthcharge)",
+		description = "Support Frigate (Missiles)",
 		energymake = 0.2,
 		energyuse = 0.2,
 		explodeas = "mediumExplosionGeneric",
